863d732e9ffc386037c5fb013767447eb324d232
Mario Chamuty <chamuty@versiontwo.sk>
2024-01-08 03:34:43 +0100

    Changes

--- index.php
+++ index.php
@@ -18,6 +18,9 @@ if (file_exists('metadata/page.json')) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400&family=Roboto+Mono:wght@100;200&display=swap"
               rel="stylesheet">
+
+        <link rel="shortcut icon"
+              href="data:image/svg+xml,%3Csvg version='1.0' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 64 64' enable-background='new 0 0 64 64' xml:space='preserve' fill='%23000000'%3E%3Cg id='SVGRepo_bgCarrier' stroke-width='0'%3E%3C/g%3E%3Cg id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'%3E%3C/g%3E%3Cg id='SVGRepo_iconCarrier'%3E%3Cg%3E%3Cpath fill='%23F9EBB2' d='M56,62H10c-2.209,0-4-1.791-4-4s1.791-4,4-4h46V62z'%3E%3C/path%3E%3Cg%3E%3Cpath fill='%2345AAB8' d='M6,4v49.537C7.062,52.584,8.461,52,10,52h2V2H8C6.896,2,6,2.896,6,4z'%3E%3C/path%3E%3Cpath fill='%2345AAB8' d='M56,2H14v50h42h2v-2V4C58,2.896,57.104,2,56,2z'%3E%3C/path%3E%3C/g%3E%3Cg%3E%3Cpath fill='%23394240' d='M60,52V4c0-2.211-1.789-4-4-4H8C5.789,0,4,1.789,4,4v54c0,3.313,2.687,6,6,6h49c0.553,0,1-0.447,1-1 s-0.447-1-1-1h-1v-8C59.104,54,60,53.104,60,52z M6,4c0-1.104,0.896-2,2-2h4v50h-2c-1.539,0-2.938,0.584-4,1.537V4z M56,62H10 c-2.209,0-4-1.791-4-4s1.791-4,4-4h46V62z M56,52H14V2h42c1.104,0,2,0.896,2,2v46v2H56z'%3E%3C/path%3E%3Cpath fill='%23394240' d='M43,26H23c-0.553,0-1,0.447-1,1s0.447,1,1,1h20c0.553,0,1-0.447,1-1S43.553,26,43,26z'%3E%3C/path%3E%3Cpath fill='%23394240' d='M49,20H23c-0.553,0-1,0.447-1,1s0.447,1,1,1h26c0.553,0,1-0.447,1-1S49.553,20,49,20z'%3E%3C/path%3E%3Cpath fill='%23394240' d='M23,16h12c0.553,0,1-0.447,1-1s-0.447-1-1-1H23c-0.553,0-1,0.447-1,1S22.447,16,23,16z'%3E%3C/path%3E%3C/g%3E%3Cpath opacity='0.2' fill='%23231F20' d='M6,4v49.537C7.062,52.584,8.461,52,10,52h2V2H8C6.896,2,6,2.896,6,4z'%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/svg%3E">
         <style>
             body {
                 background-color : #121212;
@@ -46,7 +49,7 @@ if (file_exists('metadata/page.json')) {
                 text-align    : center;
                 color         : #f8f9fa;
                 font-size     : 1.8em;
-                font-weight   : 300;
+                font-weight   : 400;
                 margin-bottom : 10px;
             }
 
@@ -55,7 +58,15 @@ if (file_exists('metadata/page.json')) {
                 color         : #adb5bd;
                 font-size     : 1.2em;
                 margin-bottom : 20px;
-                font-weight   : 100;
+                font-weight   : 300;
+            }
+
+            .light-mode .page-title {
+                color : #121212;
+            }
+
+            .light-mode .page-subtitle {
+                color : #121212;
             }
 
             .mp3-container {
@@ -83,6 +94,10 @@ if (file_exists('metadata/page.json')) {
                 margin-bottom : 5px;
             }
 
+            .light-mode .mp3-title {
+                color : white;
+            }
+
             .mp3-controls {
                 display         : flex;
                 align-items     : center;
@@ -105,7 +120,7 @@ if (file_exists('metadata/page.json')) {
 
             .progress-bar {
                 margin-left      : 10px;
-                width            : 65%;
+                width            : 55%;
                 background-color : #333333;
                 border-radius    : 5px;
                 overflow         : hidden;
@@ -114,10 +129,14 @@ if (file_exists('metadata/page.json')) {
             .progress-bar-inner {
                 height           : 10px;
                 background-color : #007bff;
-                width            : 0%;
+                width            : 0;
             }
 
             @media (max-width : 600px) {
+                .main-container {
+                    display : block;
+                }
+
                 .mp3-container {
                     border-radius : 0;
                 }
@@ -419,7 +438,7 @@ if (file_exists('metadata/page.json')) {
                 echo '<div class="mp3-item" id="item-' . $hash . '" data-filename="' . $encodedFile . '" data-duration="' . $formattedDuration . '">';
                 echo '<div class="mp3-title" onclick="toggleDescription(\'' . $hash . '\')" >' . $title;
                 if (!is_null($description)) {
-                    echo ' <span class="expand-caret" onclick="toggleDescription(\'' . $hash . '\')">&#9660;</span>';
+                    echo ' <span class="expand-caret caret-' . $hash . '" onclick="toggleDescription(\'' . $hash . '\')">&#9660;</span>';
                 }
                 echo '</div>';
                 echo '<div class="mp3-controls">';
